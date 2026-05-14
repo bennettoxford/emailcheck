d6ef222655110f6442ac96357d63b527efb57005
Dave Evans <dave.evans@thedatalab.org>
2026-05-14 10:18:36 +0100

    Remove unused stuff

--- docker/entrypoints/prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# At the moment the command is simply executed;
-# but any commands that should be run beforehand can be added here
-# (e.g. ./manage.py check or ./manage.py migrate)
-
-exec "$@"
